c607506ecc77f9640e997c2cc95f273c11970e6d
SofiLovelace <kaozztrs@gmail.com>
2022-12-02 09:57:58 -0600

    Antes de pasar la estrutura de enmascaramiento a una estructura modular

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import validator from './validator.js';
 
-console.log(validator.maskify);
-
-console.log(validator.isValid("5579100288426081"));
 
 const input = document.getElementById ("card");  // creamos una constante que almacene al objeto con id="card"
-
 const numbersValid = "0123456789"
 let numbersConcataned =""
+
+
 input.addEventListener("keydown", function(event) { // usamos sobre input una escucha de cualquier tecla que presionemos, esta tecla es el evento definido en la funcion con el parametro event 
     if(numbersValid.indexOf(event.key) !== -1){
         numbersConcataned = numbersConcataned + event.key;//vamos concatenando los numeros
@@ -24,13 +22,15 @@ input.addEventListener("keydown", function(event) { // usamos sobre input una es
             event.preventDefault(); //con este metodo detenemos el evento del teclado
         }}
     console.log(numbersConcataned.length); /// mascara
+
+    
+    // condicionales para crear la mascara 
     if (numbersConcataned.length <= 4){
         input.value = numbersConcataned;
     } else {
         if (numbersConcataned.length >= 17){
             numbersConcataned = numbersConcataned.slice (0, -1); //recortamos el digito excedente
             alert("Solo se permiten 16 digitos como maximo");
-            console.log(numbersConcataned); //imprimos de nuevo por que la linea 24 al estar antes se imprime antes de este recorte
         } else {
             let mascara = numbersConcataned.slice(0,-4);  //variable para almacenar los valores a enmascarar
             let visibility = numbersConcataned.slice(numbersConcataned.length - 4, numbersConcataned.length + 1); // variable para almacenar los numeros que dejaremos visibles
@@ -45,6 +45,12 @@ input.addEventListener("keydown", function(event) { // usamos sobre input una es
         }
     }
     })
+    
+    console.log(validator.isValid("5529931622059431"));
 
-
-
+/*const button = document.getElementById ("button");  // creamos una constante que almacene al objeto con id="button"
+//creamos un
+function click(numbersConcataned){
+validator.isValid(numbersConcataned)
+}
+button.addEventListener("click", click(numbersConcataned));*/
