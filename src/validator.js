64c61e27fd7671f32928fc775c760fa43bd7a63d
SofiLovelace <kaozztrs@gmail.com>
2022-12-06 21:07:06 -0600

    Hitos completados

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,10 @@
 const validator = {
   isValid:function(numberCard){
+    let bolean; // creamos una constante para almancenar el boleano final
     let result = 0;     //Creamos una variable numerica para almacenar el resultado "result"
     let resultLuhn = "";  //Creamos una variable que almacene los valores para resolver el algoritmo de luhn
     let resultMulti = ""; // // Crear una variable llamada resultMulti
-    for (let i=0; i < numberCard.length; i ++) {
+    for (let i=0; i < Number(numberCard.length); i ++) {
     // //  Aplicamos resultMulti = valor del indice actual
       if (i % 2 === 0){
         resultMulti = Number(numberCard.substring(i, i + 1)) * 2;
@@ -21,48 +22,32 @@ const validator = {
     // con un ciclo flor recorremos result luhn y sumamos todos los digitos, almacenamos el valor en result
     for (let i = 0; i < resultLuhn.length; i ++) {
       result = result + Number(resultLuhn.substring(i,i + 1));
-      console.log(result);
       //obtenemos el modulo de resul, si el valor es cero la tarjeta es valida
     }
     if (result % 10 === 0){
-      alert ("tu tarjeta es valida ");
+      bolean = true;
     } else {
-      alert ("tu tarjeta es invalida");
+      bolean = false;
     }
-    return result
+    return bolean
   },
   
-  maskify:function(numbersConcataned, event, objectListener){
-    const numbersValid = "0123456789";
-    if(numbersValid.indexOf(event.key) !== -1){numbersConcataned = numbersConcataned + event.key;//vamos concatenando los numeros
-      event.preventDefault(); //con este metodo detenemos el evento del teclado
-    } else {
-      if(event.key === "Backspace"){ ////creamos este primer if, para que podamos borrar elementos
-        numbersConcataned = numbersConcataned.slice (0, -1); //recortamos
-        event.preventDefault(); //con este metodo detenemos el evento del teclado
-      } else {
-        alert("Solo puedes ingresar digitos");
-        event.preventDefault(); //con este metodo detenemos el evento del teclado
-      }}
+  maskify:function(numbersConcataned){
+    //const numbersValid = "0123456789";
+    let maskAply = "" // variable para almacenar el valor final
     // condicionales para crear la mascara 
     if (numbersConcataned.length <= 4){
-      objectListener.value = numbersConcataned;
-    } else {
-      if (numbersConcataned.length >= 17){
-        numbersConcataned = numbersConcataned.slice (0, -1); //recortamos el digito excedente
-        alert("Solo se permiten 16 digitos como maximo");
-      } else {
-        let mascara = numbersConcataned.slice(0,-4);  //variable para almacenar los valores a enmascarar
-        let visibility = numbersConcataned.slice(numbersConcataned.length - 4, numbersConcataned.length + 1); // variable para almacenar los numeros que dejaremos visibles
-        let remplazados = ""//variable en donde remplazaremos los numeros por *
-        for (let i = 1; i <= mascara.length; i ++) { //ciclo for para crear el string con puros * de acuerdo al length de mascara
-          remplazados = remplazados + "*"
-        }
-        objectListener.value = remplazados + visibility;//ingresamos en el imput la mascara y los numeros visibles
+      maskAply = numbersConcataned;
+    } else{
+      const mascara = numbersConcataned.slice(0,-4);  //variable para almacenar los valores a enmascarar
+      const visibility = numbersConcataned.slice(numbersConcataned.length - 4, numbersConcataned.length + 1); // variable para almacenar los numeros que dejaremos visibles
+      let remplazados = ""//variable en donde remplazaremos los numeros por *
+      for (let i = 1; i <= mascara.length; i ++) { //ciclo for para crear el string con puros * de acuerdo al length de mascara
+        remplazados = remplazados + "#"
       }
+      maskAply = remplazados + visibility
     }
-    console.log(numbersConcataned);
-    return numbersConcataned
+    return maskAply
   }
 };
 export default validator;
