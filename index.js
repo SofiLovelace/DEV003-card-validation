ca269effa079dd26a2fcb16546c160510a8d4307
SofiLovelace <kaozztrs@gmail.com>
2022-12-07 18:00:14 -0600

    Updates

--- index.js
+++ index.js
@@ -9,18 +9,15 @@ button.addEventListener("click", function(event){
   const month = document.getElementById ("expirationMonth").value;
   const year =  document.getElementById ("expirationYear").value;
   const cvv = document.getElementById ("cvv").value;
-  const totalLength = month.length + year.length + cvv.length;
-  if (totalLength === 7) {
+  const name = document.getElementById ("headline").value;
+  if (name.length + cvv.length + year.length + month.length > 7) {
     if (numbersConcataned.length === 16) {    //condicionamos la ejecución de is valid a 16 caracteres en el input
-    
       if (validator.isValid(numbersConcataned) === true){
         alert ("Tu tarjeta es valida tu compra se esta procesando");
       }
       else {
         alert ("Ingresa una tarjeta valida para continuar");
-      }
-      
-    }
+      }}
     else {
       alert ("Ingresa los 16 digitos de tu plastico");    //si la condicion no se cumple
     } 
@@ -32,23 +29,17 @@ button.addEventListener("click", function(event){
 
 const input = document.getElementById ("card");  // creamos una constante que almacene al objeto con id="card"
 input.addEventListener("keydown", function(event){ //escuchamos los eventos del teclado sobre card
+  event.preventDefault(); //con este metodo detenemos el evento del teclado
   if(event.key === "Backspace"){ ////creamos este primer if, para que podamos borrar elementos
     numbersConcataned = numbersConcataned.slice (0, -1); //recortamos
-    event.preventDefault(); //con este metodo detenemos el evento del teclado
   } 
   if (numbersConcataned.length <= 15) {
     const numbersValid = "0123456789"; // declaramos los numeros validos
     if(numbersValid.indexOf(event.key) !== -1){
       numbersConcataned = numbersConcataned + event.key;//vamos concatenando los numeros
-      event.preventDefault(); //con este metodo detenemos el evento del teclado
     } 
-    else {
-      event.preventDefault(); //con este metodo detenemos el evento del teclado
-    }
-    input.value = validator.maskify(numbersConcataned);//ingresamos en el input el return de maskify
   } else {
-    event.preventDefault();
-    input.value = validator.maskify(numbersConcataned);
     alert ("Solo puedes ingresar 16 digitos")
   }
+  input.value = validator.maskify(numbersConcataned);//ingresamos en el input el return de maskify
 }); 
--- validator.js
+++ validator.js
@@ -1,42 +1,28 @@
 const validator = {
   isValid:function(numberCard){
-    let bolean; // creamos una constante para almancenar el boleano final
     let result = 0;     //Creamos una variable numerica para almacenar el resultado "result"
     let resultLuhn = "";  //Creamos una variable que almacene los valores para resolver el algoritmo de luhn
     let resultMulti = ""; // // Crear una variable llamada resultMulti
     for (let i=0; i < Number(numberCard.length); i ++) {
-    // //  Aplicamos resultMulti = valor del indice actual
       if (i % 2 === 0){
         resultMulti = Number(numberCard.substring(i, i + 1)) * 2;
-      // // // Aplicamos un condicional si resultMulti>= 10, entonces aplicamos una suma de sus digitos
       } else {
         resultMulti = Number(numberCard.substring(i,i + 1));
-      // // Siempre y cuando el indice divido entre 2 arrojo como modulo 0 entonces multiplicamos por 2, almacenamos el valor en la variable resultMulti
       }
       if (resultMulti >= 10) { 
         resultMulti = ((resultMulti-(resultMulti % 10)) / 10) + (resultMulti % 10);
       } 
-      // // Concatenamos en resultLuhn los valores de resultMulti en cada iteracion
       resultLuhn = resultLuhn + resultMulti;
     }
-    // con un ciclo flor recorremos result luhn y sumamos todos los digitos, almacenamos el valor en result
     for (let i = 0; i < resultLuhn.length; i ++) {
       result = result + Number(resultLuhn.substring(i,i + 1));
-      //obtenemos el modulo de resul, si el valor es cero la tarjeta es valida
     }
-    if (result % 10 === 0){
-      bolean = true;
-    } else {
-      bolean = false;
-    }
-    return bolean
+    return result % 10 === 0;
   },
   
   maskify:function(numbersConcataned){
-    //const numbersValid = "0123456789";
     let maskAply = "" // variable para almacenar el valor final
-    // condicionales para crear la mascara 
-    if (numbersConcataned.length <= 4){
+    if (numbersConcataned.length <= 4){ // condicionales para crear la mascara 
       maskAply = numbersConcataned;
     } else{
       const mascara = numbersConcataned.slice(0,-4);  //variable para almacenar los valores a enmascarar
@@ -48,6 +34,5 @@ const validator = {
       maskAply = remplazados + visibility
     }
     return maskAply
-  }
-};
+  }};
 export default validator;
